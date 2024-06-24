f508a35246e473c2ffabd2e6fb251cc80b639c7a
Soy Steven <steven13merchan@gmail.com>
2024-06-24 09:47:19 -0500

    optimizando el codigo js

--- js/voice-search.js
+++ js/voice-search.js
@@ -1,18 +1,18 @@
 
-const voiceSearch = document.querySelector(".voice-search");
+const voiceSearch = document.querySelector(".voice-search").style;
 let microAceptado = false;
 
 const voiceSearchModalOpen = ()=>{
-	voiceSearch.style.display = "flex";
-	voiceSearch.style.animation = "aparecer 0.5s forwards";
+	voiceSearch.display = "flex";
+	voiceSearch.animation = "aparecer 0.5s forwards";
 	voiceRecognition();
 }
 
 
 const voiceSearchModalClose = () =>{
-	voiceSearch.style.animation = "desaparecer 0.25s forwards";
+	voiceSearch.animation = "desaparecer 0.25s forwards";
 	setTimeout(()=>{
-	    voiceSearch.style.display = "none";
+	    voiceSearch.display = "none";
 	},250)
 }
 
